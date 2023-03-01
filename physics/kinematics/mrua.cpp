b081b9d58dc7224af506bbfb1d0c558feeeb7342
ferrandusqui31 <ferran.marpas@gmail.com>
2023-03-01 22:44:05 +0100

    Core functions transfered to mrua.h

--- physics/kinematics/mrua.cpp
+++ physics/kinematics/mrua.cpp
@@ -1,22 +1,7 @@
 // File to calculate 1-dimension rectilinear movement accelerated.
 #include <iostream>
-#include <math.h>
+#include "mrua.h"
 using namespace std;
-
-// Movement equation: x = x0 + v0*t + 1/2*a^2
-float mruaMov(float x0, float t, float v0, float a){
-    return x0 + v0*t + .5*a*t*t;
-}
-
-// Velocity equation: vf = v0 + a*t
-float mruaVel(float t, float v0, float a){
-    return v0 + a*t;
-}
-
-// Quadratic equation: vf^2 = v0^2 + 2*a*x
-float mruaQua(float x, float v0, float a){
-    return sqrt(v0*v0 + 2*x*a);
-}
 
 // Execute the function preferred by the user
 void mruaUI(){
@@ -37,21 +22,21 @@ void mruaUI(){
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
 
-        cout << endl << "x: " << mruaMov(x0, t, v, a) << "m" << endl;
+        cout << endl << "x: " << fkphy::mruaMov(x0, t, v, a) << "m" << endl;
     }
     else if(in == "vel" or in == "v"){
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
         cout << "t: "; cin >> t;
 
-        cout << endl << "vf: " << mruaVel(t, v, a) << "m/s" << endl;
+        cout << endl << "vf: " << fkphy::mruaVel(t, v, a) << "m/s" << endl;
     }
     else if(in == "qua" or in == "q"){
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
         cout << "x: "; cin >> x0;
 
-        cout << endl << "vf: " << mruaQua(x0, v, a) << "m/s" << endl;
+        cout << endl << "vf: " << fkphy::mruaQua(x0, v, a) << "m/s" << endl;
     }
     else{   // In case the user doesn't write any possibility
         cout << "Please type a valid command. If you need to see them type 'help'" << endl;
