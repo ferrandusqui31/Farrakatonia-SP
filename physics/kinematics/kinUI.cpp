70470e30a5c73118d53334289e919d12e63ec2ec
ferrandusqui31 <ferran.marpas@gmail.com>
2023-03-01 22:57:00 +0100

    Changed file names to be more global

--- physics/kinematics/kinUI.cpp
+++ physics/kinematics/kinUI.cpp
@@ -1,11 +1,11 @@
 // File to calculate 1-dimension rectilinear movement accelerated.
 #include <iostream>
-#include "mrua.h"
+#include "kin.h"
 using namespace std;
 
 // Execute the function preferred by the user
-void mruaUI(){
-    float x0, x, t, v, a;
+void kinUI(){
+    float x, t, v, a;   // Using x as x0 and Δx since there's not the necessity of having more than one. If necessary add more variables.
     string in;
     cin >> in;
     cout << endl;
@@ -13,16 +13,16 @@ void mruaUI(){
     if(in == "help"){
         cout << "mov | m - Movement equation (x = x0 + v0*t + 1/2*a^2)" << endl;
         cout << "vel | v - Velocity equation (vf = v0 + a*t)" << endl;
-        cout << "qua | q - Quadratic equation (vf^2 = v0^2 + 2*a*x)" << endl << endl;
-        mruaUI();
+        cout << "qua | q - Quadratic equation (vf^2 = v0^2 + 2*a*Δx)" << endl << endl;
+        kinUI();
     }
     else if(in == "mov" or in == "m"){
-        cout << "x0: "; cin >> x0;
+        cout << "x0: "; cin >> x;
         cout << "t: "; cin >> t;
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
 
-        cout << endl << "x: " << fkphy::mruaMov(x0, t, v, a) << "m" << endl;
+        cout << endl << "x: " << fkphy::mruaMov(x, t, v, a) << "m" << endl;
     }
     else if(in == "vel" or in == "v"){
         cout << "v0: "; cin >> v;
@@ -34,17 +34,17 @@ void mruaUI(){
     else if(in == "qua" or in == "q"){
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
-        cout << "x: "; cin >> x0;
+        cout << "Δx: "; cin >> x;
 
-        cout << endl << "vf: " << fkphy::mruaQua(x0, v, a) << "m/s" << endl;
+        cout << endl << "vf: " << fkphy::mruaQua(x, v, a) << "m/s" << endl;
     }
     else{   // In case the user doesn't write any possibility
         cout << "Please type a valid command. If you need to see them type 'help'" << endl;
-        mruaUI();
+        kinUI();
     }
 }
 
 int main(){
     cout << "To see the comands type 'help'. Enter the data in the SI." << endl;
-    mruaUI();
+    kinUI();
 }
