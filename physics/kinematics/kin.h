836f60bfbb2b10247bf93a409130e1f3f8cbffef
ferrandusqui31 <ferran.marpas@gmail.com>
2023-03-02 23:04:03 +0100

    Added parabolic movement

--- physics/kinematics/kin.h
+++ physics/kinematics/kin.h
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <array>
 using namespace std;
 
 namespace fkphy{
@@ -17,6 +18,13 @@ float d1Vel(float t, float v0, float a){
 // Quadratic equation: vf^2 = v0^2 + 2*a*x
 float d1Qua(float x, float v0, float a){
     return sqrt(v0*v0 + 2*x*a);
+}
+
+// Parabolic movement
+array<float, 2> d2Par(float x0, float y0, float v0x, float v0y, float a, float g, float t){
+    float x = d1Mov(x0, t, v0x, a);
+    float y = d1Mov(y0, t, v0y, -g);
+    return {x,y};
 }
 
 }
--- physics/kinematics/kinUI.cpp
+++ physics/kinematics/kinUI.cpp
@@ -1,5 +1,6 @@
 // File to calculate 1-dimension rectilinear movement accelerated.
 #include <iostream>
+#include <vector>
 #include "kin.h"
 using namespace std;
 
@@ -13,7 +14,7 @@ float isEnter(){
 
 // Execute the function preferred by the user
 void kinUI(){
-    float x, t, v, a;   // Using x as x0 and Δx since there's not the necessity of having more than one. If necessary add more variables.
+    float x, y, t, vx, vy, a, g;   // Using x as x0 and Δx since there's not the necessity of having more than one. If necessary add more variables.
     string in;
     cin >> in;
     cout << endl;
@@ -21,27 +22,41 @@ void kinUI(){
     if(in == "help"){
         cout << "mov | m - Movement equation (x = x0 + v0*t + 1/2*a^2)" << endl;
         cout << "vel | v - Velocity equations (vf = v0 + a*t) | (vf^2 = v0^2 + 2*a*Δx)" << endl;
+        cout << "par | p - Parabolic movement. Enter the gravity in positive" << endl;
         cout << endl;
         kinUI();
     }
     else if(in == "mov" or in == "m"){
         cout << "x0: "; cin >> x;
         cout << "t: "; cin >> t;
-        cout << "v0: "; cin >> v;
+        cout << "v0: "; cin >> vx;
         cout << "a: "; cin >> a;
 
-        cout << endl << "x: " << fkphy::d1Mov(x, t, v, a) << "m" << endl;
+        cout << endl << "x: " << fkphy::d1Mov(x, t, vx, a) << "m" << endl;
     }
     else if(in == "vel" or in == "v"){
-        cout << "v0: "; cin >> v;
+        cout << "v0: "; cin >> vx;
         cout << "a: "; cin >> a;
         cin.ignore();   // Used because the program was skipping next cin
         cout << "t: "; t = isEnter();   // Optional value
         cout << "x|Δx: "; x = isEnter();    // Optional value
 
-        if(t != '\0') cout << endl << "vf: " << fkphy::d1Vel(t, v, a) << "m/s" << endl;
-        else if(x != '\0') cout << endl << "vf: " << fkphy::d1Qua(x, v, a) << "m/s" << endl;
+        if(t != '\0') cout << endl << "vf: " << fkphy::d1Vel(t, vx, a) << "m/s" << endl;
+        else if(x != '\0') cout << endl << "vf: " << fkphy::d1Qua(x, vx, a) << "m/s" << endl;
         else cout << "You must provide t or x" << endl;
+    }
+    else if(in == "par" or in == "p"){
+        cout << "x0: "; cin >> x;
+        cout << "y0: "; cin >> y;
+        cout << "vx0: "; cin >> vx;
+        cout << "vy0: "; cin >> vy;
+        cout << "a: "; cin >> a;
+        cout << "g: "; cin >> g;
+        cout << "t: "; cin >> t;
+
+        array<float, 2> result = fkphy::d2Par(x, y, vx, vy, a, g, t);
+        cout << "x: " << result[0] << "m" << endl;
+        cout << "y: " << result[1] << "m" << endl;
     }
     else{   // In case the user doesn't write any possibility
         cout << "Please type a valid command. If you need to see them type 'help'" << endl;
