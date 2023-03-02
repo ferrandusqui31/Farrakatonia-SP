889846e7fa1dbd1ed62a450d4b7775972cef95ed
ferrandusqui31 <ferran.marpas@gmail.com>
2023-03-02 18:04:42 +0100

    Simplified the program for the user

--- physics/kinematics/kinUI.cpp
+++ physics/kinematics/kinUI.cpp
@@ -3,6 +3,14 @@
 #include "kin.h"
 using namespace std;
 
+// Function for the values that can be skipped
+float isEnter(){
+    string input;
+    getline(cin, input);    // Way to get inputs but can get ENTER
+    if(input == "") return '\0';    // '\0' is equivalent to NULL
+    else return stof(input);
+}
+
 // Execute the function preferred by the user
 void kinUI(){
     float x, t, v, a;   // Using x as x0 and Δx since there's not the necessity of having more than one. If necessary add more variables.
@@ -12,8 +20,8 @@ void kinUI(){
 
     if(in == "help"){
         cout << "mov | m - Movement equation (x = x0 + v0*t + 1/2*a^2)" << endl;
-        cout << "vel | v - Velocity equation (vf = v0 + a*t)" << endl;
-        cout << "qua | q - Quadratic equation (vf^2 = v0^2 + 2*a*Δx)" << endl << endl;
+        cout << "vel | v - Velocity equations (vf = v0 + a*t) | (vf^2 = v0^2 + 2*a*Δx)" << endl;
+        cout << endl;
         kinUI();
     }
     else if(in == "mov" or in == "m"){
@@ -27,16 +35,13 @@ void kinUI(){
     else if(in == "vel" or in == "v"){
         cout << "v0: "; cin >> v;
         cout << "a: "; cin >> a;
-        cout << "t: "; cin >> t;
+        cin.ignore();   // Used because the program was skipping next cin
+        cout << "t: "; t = isEnter();   // Optional value
+        cout << "x|Δx: "; x = isEnter();    // Optional value
 
-        cout << endl << "vf: " << fkphy::mruaVel(t, v, a) << "m/s" << endl;
-    }
-    else if(in == "qua" or in == "q"){
-        cout << "v0: "; cin >> v;
-        cout << "a: "; cin >> a;
-        cout << "Δx: "; cin >> x;
-
-        cout << endl << "vf: " << fkphy::mruaQua(x, v, a) << "m/s" << endl;
+        if(t != '\0') cout << endl << "vf: " << fkphy::mruaVel(t, v, a) << "m/s" << endl;
+        else if(x != '\0') cout << endl << "vf: " << fkphy::mruaQua(x, v, a) << "m/s" << endl;
+        else cout << "You must provide t or x" << endl;
     }
     else{   // In case the user doesn't write any possibility
         cout << "Please type a valid command. If you need to see them type 'help'" << endl;
