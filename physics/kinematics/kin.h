789607cc6db6f704842c4527d471c7636642a866
ferrandusqui31 <ferran.marpas@gmail.com>
2023-03-02 20:15:31 +0100

    Variable names change

--- physics/kinematics/kin.h
+++ physics/kinematics/kin.h
@@ -3,18 +3,19 @@ using namespace std;
 
 namespace fkphy{
 
+// Straight line movement
 // Movement equation: x = x0 + v0*t + 1/2*a^2
-float strMov(float x0, float t, float v0, float a){
+float d1Mov(float x0, float t, float v0, float a){
     return x0 + v0*t + .5*a*t*t;
 }
 
 // Velocity equation: vf = v0 + a*t
-float strVel(float t, float v0, float a){
+float d1Vel(float t, float v0, float a){
     return v0 + a*t;
 }
 
 // Quadratic equation: vf^2 = v0^2 + 2*a*x
-float strQua(float x, float v0, float a){
+float d1Qua(float x, float v0, float a){
     return sqrt(v0*v0 + 2*x*a);
 }
 
